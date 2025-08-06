82347914cf6a25f7968a7df01a0a726fe45155d3
valentinkpl <valou.kpl@gmail.com>
2025-08-06 15:06:02 +0200

    version finale avec debugage

--- app/page.js
+++ app/page.js
@@ -1,6 +1,11 @@
 "use client";
+import { Suspense } from "react";
 import WeatherClientComponent from "../components/WeatherClientComponent";
 
 export default function HomePage() {
-  return <WeatherClientComponent />;
+  return (
+    <Suspense fallback={<div className="text-center mt-10 text-gray-600">Chargement météo...</div>}>
+      <WeatherClientComponent />
+    </Suspense>
+  );
 }
